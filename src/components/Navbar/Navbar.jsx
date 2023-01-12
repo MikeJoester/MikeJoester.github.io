925fdfba23732e6976c6f803b0b47add0e9abe5c
MikeJoester <dan.nguyen200206@vnuk.edu.vn>
2023-01-12 11:15:53 +0700

    .

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
         <div class="container flex flex-wrap justify-between items-center mx-auto">
-            <a href="/home" class="flex items-center">
+            <a href="/" class="flex items-center">
                 <img src="https://cdn-icons-png.flaticon.com/512/2949/2949874.png" class="mr-3 h-6 sm:h-10" alt="Blog's Logo" />
                 <span class="self-center text-3xl font-semibold whitespace-nowrap dark:text-white">My Blog</span>
             </a>
